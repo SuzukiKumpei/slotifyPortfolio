689fefd2dadb246f93aa0871a8e00da989e4b785
SuzukiKumpei <51525557+SuzukiKumpei@users.noreply.github.com>
2019-06-09 18:01:47 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("ASIA/TOKYO");
 
-	$con = mysqli_connect("localhost", "root", "", "slotify");
+	$con = mysqli_connect("localhost", "root", "cO7HZoMBuy9dYQoT", "slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
