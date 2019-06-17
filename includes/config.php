68814370020a1d9580ce1eb8613c114da96ccc3b
SuzukiKumpei <51525557+SuzukiKumpei@users.noreply.github.com>
2019-06-17 15:31:44 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("ASIA/TOKYO");
 
-	$con = mysqli_connect("localhost", "root", "cO7HZoMBuy9dYQoT", "slotify");
+	$con = mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "b965d64a76f1b0", "32bffce9", "heroku_ef7f5355a7e1ce2");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
